a5f37a6e7471f28c6bbd190d1b00473b474929f4
fabian <fabiansta@protonmail.com>
2022-09-22 18:32:19 +0200

    fixed lighting firmware

--- firmware/plain60rp2040/rules.mk
+++ firmware/plain60rp2040/rules.mk
@@ -6,21 +6,13 @@ BOOTLOADER = rp2040
 # Build Options
 #   change yes to no to disable
 #
+
+ALLOW_WARNINGS = yes 
+LTO_ENABLE = no 
+WS2812_DRIVER = vendor 
+RGBLIGHT_ENABLE = yes 
+AUDIO_ENABLE = no 
+MOUSEKEY_ENABLE = yes 
+EXTRAKEY_ENABLE = yes 
 ENCODER_ENABLE = no 
-ENCODER_MAP_ENABLE = no
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output
-
-RGB_MATRIX_ENABLE = no
-
+NKRO_ENABLE = yes 
